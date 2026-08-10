77d0780aea01c7607de8dd9d0863509f37d9d4f1
snehalshmane <snehal.mane@thoughtworks.com>
2026-08-10 14:46:05 +0530

    share modules between container and marketing

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
+const packageJson = require('../package.json');
+
 const devConfig = {
     mode: 'development',
     devServer: {
@@ -17,7 +19,8 @@ const devConfig = {
             filename: 'remoteEntry.js',
             exposes: {
                 './MarketingApp': "./src/bootstrap"
-            }
+            },
+            shared: packageJson.dependencies,
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
