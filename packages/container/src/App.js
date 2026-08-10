f788d6b6eeee5e233ef6ba749327d3f8f708a504
snehalshmane <snehal.mane@thoughtworks.com>
2026-08-10 14:35:42 +0530

    generic integration between container and marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import {mount} from 'marketing/MarketingApp'
-
-console.log(mount)
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    return <h1>Hello</h1>
+    return <div>
+        <h1>Hello!</h1>
+        <hr/>
+        <MarketingApp/>
+    </div>
 }
