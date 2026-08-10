c1a99d87ef583b5c1fa2d81ae244d1e4997bd672
Snehal Mane <snehalmane@Snehals-MacBook-Pro.local>
2026-08-10 14:06:37 +0530

    Assemble App component for Marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const mount = (el) => {
     ReactDOM.render(
-        <h1>Hi</h1>,
+        <App/>,
         el
     );
 };
