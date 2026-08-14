5c0daaeb18531786c4f2866f083a5d1eb771a607
snehalshmane <snehal.mane@thoughtworks.com>
2026-08-14 14:56:29 +0530

    add progress bar

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,6 @@
 import React, {Suspense} from "react";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {createGenerateClassName} from "@material-ui/core/styles";
 import {StylesProvider} from "@material-ui/core";
@@ -14,7 +15,7 @@ export default () => {
         <StylesProvider generateClassName={generateClassName}>
             <div>
                 <Header/>
-                <Suspense fallback={<div>Loading...</div>}>
+                <Suspense fallback={<Progress/>}>
                     <Switch>
                         <Route path="/auth" component={AuthLazy}/>
                         <Route path="/" component={MarketingLazy}/>
