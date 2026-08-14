10836d65ce1079e5b739a1644192c8a1531baa39
snehalshmane <snehal.mane@thoughtworks.com>
2026-08-14 12:13:44 +0530

    use memory history for marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createMemoryHistory} from 'history';
 
 const mount = (el) => {
+    const history = createMemoryHistory();
     ReactDOM.render(
-        <App/>,
+        <App history={history}/>,
         el
     );
 };
