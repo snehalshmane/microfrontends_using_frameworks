7fe322dc849aa44fcc7ff4c42723b9eef984f0a3
snehalshmane <snehal.mane@thoughtworks.com>
2026-08-14 13:14:40 +0530

    init auth frontend

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {createBrowserHistory, createMemoryHistory} from 'history';
+
+const mount = (el, {
+    onNavigate, defaultHistory
+}) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(
+        <App history={history}/>,
+        el
+    );
+
+    return {
+        onParentNavigate({pathname: nextPathname}) {
+            const {pathname} = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector("#_auth-dev-root");
+    if (el) {
+        mount(el, {
+            defaultHistory: createBrowserHistory(),
+        });
+    }
+}
+
+export {mount};
