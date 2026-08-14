c972801bf57a679238718a105bacb6584f8381a4
snehalshmane <snehal.mane@thoughtworks.com>
2026-08-14 15:59:38 +0530

    pass auth state and change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, {Suspense} from "react";
+import React, {Suspense, useState} from "react";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
@@ -11,14 +11,20 @@ const AuthLazy = React.lazy(() => import('./components/AuthApp'));
 const generateClassName = createGenerateClassName({productionPrefix: "co"});
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return <BrowserRouter>
         <StylesProvider generateClassName={generateClassName}>
             <div>
-                <Header/>
+                <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)}/>
                 <Suspense fallback={<Progress/>}>
                     <Switch>
-                        <Route path="/auth" component={AuthLazy}/>
-                        <Route path="/" component={MarketingLazy}/>
+                        <Route path="/auth">
+                            <AuthLazy onSignIn={() => setIsSignedIn(true)}/>
+                        </Route>
+                        <Route path="/">
+                            <MarketingLazy/>
+                        </Route>
                     </Switch>
                 </Suspense>
             </div>
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import {mount} from 'auth/AuthApp'
 import React, {useEffect, useRef} from 'react'
 import {useHistory} from 'react-router-dom';
 
-export default () => {
+export default ({onSignIn}) => {
     const ref = useRef(null);
     const history = useHistory();
 
@@ -15,9 +15,7 @@ export default () => {
                     history.push(nextPathname);
                 }
             },
-            onSignIn: () => {
-                console.log("User is logged in");
-            }
+            onSignIn
         });
 
         history.listen(onParentNavigate);
