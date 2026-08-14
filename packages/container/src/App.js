b445bdc145e73a35add1c3f3dbdaedb596457a49
snehalshmane <snehal.mane@thoughtworks.com>
2026-08-14 13:26:38 +0530

    wire up auth

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {createGenerateClassName} from "@material-ui/core/styles";
 import {StylesProvider} from "@material-ui/core";
+import AuthApp from "./components/AuthApp";
 
 const generateClassName = createGenerateClassName({productionPrefix: "co"});
 
@@ -12,7 +13,10 @@ export default () => {
         <StylesProvider generateClassName={generateClassName}>
             <div>
                 <Header/>
-                <MarketingApp/>
+                <Switch>
+                    <Route path="/auth" component={AuthApp}/>
+                    <Route path="/" component={MarketingApp}/>
+                </Switch>
             </div>
         </StylesProvider>
     </BrowserRouter>
